159fee68ad3d0da46b0424bf74c280d18fb6a6d5
developersview <pcslg1998@gmail.com>
2023-05-04 12:58:45 +0530

    springboot jwt boilerplate code

---
+++ springboot-jwt-product-api/src/main/java/com/developersview/springbootjwtproductapi/SpringbootJwtProductApiApplication.java
@@ -0,0 +1,13 @@
+package com.developersview.springbootjwtproductapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJwtProductApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJwtProductApiApplication.class, args);
+	}
+
+}
---
+++ springboot-jwt-product-api/src/test/java/com/developersview/springbootjwtproductapi/SpringbootJwtProductApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developersview.springbootjwtproductapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJwtProductApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
